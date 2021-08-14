cbf979fe2336b63d58e3a60f40a3d8d6065048ad
QQQQM <113231780@qq.com>
2021-08-14 08:43:03 -0700

    增加对本地不存在镜像的自动拉取

--- Readme.md
+++ Readme.md
@@ -8,3 +8,5 @@ python main.py image_name(=busybox) [select_num = 0]
 获取镜像的history信息并保存为"History_$image_name.json"格式
 ## select_num = 2
 获取镜像的inspect信息并保存为"Inspect_$image_name.json"格式
+
+### * 如果不存在本地镜像将会自动拉取对应的镜像
--- main.py
+++ main.py
@@ -2,34 +2,49 @@ import sys
 import json
 import docker
 
-def pull_image(image_name):
-    print("hello")
+def print_in_json (data):
+    json_str = json.dumps(data, sort_keys=True, indent=2)
+    print(json_str)
 
-def test(image_name):
-    print(image_name)
-    client = docker.from_env()
-    
+
+def pull_image(image_name,client):
+    print("pulling...")
     try:
-        print(client.images.list(all = True))
-    except docker.errors.ImageNotFound as e:
-        print("error:",e)
-        pull_image(image_name)
-    finally:
-        print("end")
+        client.images.pull(image_name, tag=None,all_tags=False)
+    except docker.errors.APIError as err:
+        print(err)
+        return err
+    print(image_name, "pulled!")
+    return None
 
+def test_if_exist(image_name):
+    print("dealing with :", image_name)
+    client = docker.from_env()
+    try:
+        data = client.images.get(image_name)
+        print(data)
+    except docker.errors.ImageNotFound as e:
+        print("可能本地不存在", image_name, "镜像，出现 error:", e , "\n")
+        err = pull_image(image_name,client)
+        if err != None:
+            return err
+        return None
+    return None
 
 
 def get_detail(image_name,function,type_name):
+    print("Getting", type_name, "information...")
     filename = type_name + image_name + ".json"
     data = function(image_name)
-    json_str = json.dumps(data, sort_keys=True, indent=2)
-    print(json_str)
     with open(filename,"w") as file_obj:
         json.dump(data,file_obj,sort_keys=True,indent=2)
     file_obj.close()
+    # print_in_json(data)
+    print("done!")
 
 
 def get_imageid(image_name):
+    print("Getting layer information...")
     layer_filename = "Layer_" + image_name + ".txt"
     layer_file = open(layer_filename,"w")
 
@@ -41,11 +56,15 @@ def get_imageid(image_name):
         layer_file.write("Layer" + str(cnt) + ":" + i + "\n")
     layer_file.write("Image Id:" + data["Id"])
     layer_file.close()
+    print("Done!")
 
 def main():
     image_name = "busybox" if len(sys.argv)  == 1 else sys.argv[1]
     select = int(sys.argv[2]) if len(sys.argv)  == 3 else 0
     client = docker.from_env()
+    err = test_if_exist(image_name)
+    if err != None:
+        return
     
     if select == 0:
         get_imageid(image_name)
@@ -54,7 +73,7 @@ def main():
     elif select ==2:
         get_detail(image_name, client.api.inspect_image, "Inspect_")
     else:
-        test(image_name)
+        pass
 
 if __name__ == "__main__":
 
